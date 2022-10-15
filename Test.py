08cb50fc510da4d8850313fc85f2a198a84aa90e
Shivank Srivastava <74642182+ShivankSrivastava@users.noreply.github.com>
2022-10-14 20:26:56 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,10 +2,6 @@ import unittest
 
 from HW4a import connect
 
-# This code implements the unit test functionality
-# https://docs.python.org/3/library/unittest.html has a nice description of the framework
-
-
 class TesConnect(unittest.TestCase):
     def testConnect(self):
         self.assertEqual(connect('?'), False)
